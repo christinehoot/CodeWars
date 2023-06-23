056d0e072eb89020564155f93bbb042e4c4c90ff
Christine Marie Huotari <98130829+christinehoot@users.noreply.github.com>
2023-06-23 13:48:06 -0500

    Add files via upload

---
+++ codewarssolution005.py
@@ -0,0 +1,8 @@
+# Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.
+
+def get_char(c):
+    x = chr(c)
+    return x
+
+
+print(get_char(65))
