e1c2b6cbad8906d0337c4ba3f30c6d0f38198654
Christine Marie Huotari <98130829+christinehoot@users.noreply.github.com>
2024-01-26 06:29:21 -0600

    Add files via upload

---
+++ codewarssolution005.sql
@@ -0,0 +1,4 @@
+--Complete the solution so that it reverses the string passed into it.
+-- # write your SQL statement here: you are given a table 'solution' with column 'str', return a table with column 'str' and your result in a column named 'res'.
+SELECT str, REVERSE(str) AS res
+FROM solution;
